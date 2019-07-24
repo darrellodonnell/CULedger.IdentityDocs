4e9a0f7a457f8fb8c949113e288b4f852063c808
darrellodonnell <darrell.odonnell@continuumloop.com>
2019-07-24 05:09:25 -0400

    created C# heartbeatexample

---
+++ oauth/example.NET/CULedger.IdentityOAuthSample/CULedger.IdentityOAuthSample/AccessToken.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CULedger.IdentityOAuthSample
+{
+    public class AccessToken
+    {
+        public string access_token { get; set; }
+        public string token_type { get; set; }
+        public long expires_in { get; set; }
+    }
+}
